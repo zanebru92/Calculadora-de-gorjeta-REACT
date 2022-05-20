0d382b9b365c8061ec0bdab64292f2c8587b5bf7
Bruno Zanella <zanella.bruno@gmail.com>
2022-05-20 11:18:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import styled from "styled-components";
+
+const Titulo = styled.h1`
+  font-size: 3rem;
+  text-transform: capitalize;
+`
+
+
+ const Input = styled.input`
+  width: 20rem;
+  height: 2rem;
+  font-size: 1rem;
+  border: 1px solid #000
+  padding: 1rem;
+  margin-bottom: 2rem;
+ `
+
 
 function App() {
+  
+  const [total, setTotal] = useState(0);
+  const [gorjeta, setGorjeta] = useState(10);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Titulo>calculadora de gorjeta</Titulo>
+      <p>Digite o valor total :</p>
+      <Input
+        placeholder="R$"
+        type="number"
+        value={total}
+        onChange={(e) => setTotal(parseFloat(e.target.value))}
+      />
+      <p>Digite a porcentagem da gorjeta:</p>
+      <Input
+        placeholder="%"
+        type="number"
+        value={gorjeta}
+        onChange={(e) => setGorjeta(parseFloat(e.target.value))}
+      />
+      <hr/>
+      
+      {total > 0 &&
+      <>
+      <p>Subtotal: R${total}</p>
+      <p>Gorjeta: R${total * (gorjeta / 100)}</p>
+      <h3>Total: R${total + (gorjeta/100) * total} </h3>
+      </>
+      }
+    </>
   );
 }
 
